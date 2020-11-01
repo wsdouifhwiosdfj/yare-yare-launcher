504e769d119e6a463572508cf502af660f818332
M4teus_not_the_duckhead <66799704+wsdouifhwiosdfj@users.noreply.github.com>
2020-11-01 20:03:03 -0300

    Add files via upload

---
+++ Module.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Mod
+{
+    public class Module : ETGModule
+    {
+        public static readonly string MOD_NAME = "yari launcher rename";
+        public static readonly string VERSION = "0.0.0";
+        public static readonly string TEXT_COLOR = "#00FFFF";
+
+        public override void Start()
+        {
+            Log($"{MOD_NAME} v{VERSION} didn't crash etg hooray.", TEXT_COLOR);
+            Gun gun3 = PickupObjectDatabase.GetById(16) as Gun;
+            gun3.SetName("Yare yare launcher");
+        }
+
+        public static void Log(string text, string color="FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+
+        public override void Exit() { }
+        public override void Init() { }
+    }
+}
